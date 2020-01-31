804225dc0a031c2824a9a1cad385dfc7d2f7639a
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-01-31 09:45:46 -0700

    Change classes.js to fix redeclaration error
    At least in index.html

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,6 +1,6 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-class CuboidMaker {
+class MyCuboidMaker {
     constructor(length, width, height) {
         this.length = length;
         this.width = width;
@@ -17,14 +17,14 @@ class CuboidMaker {
 
 }
 
-let cuboid = new CuboidMaker(4, 5, 5);
+let myCuboid = new MyCuboidMaker(4, 5, 5);
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(myCuboid.volume()); // 100
+console.log(myCuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
-class CubeMaker extends CuboidMaker {
+class CubeMaker extends MyCuboidMaker {
     constructor(size) {
         super(size, size, size);
     }
